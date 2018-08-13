23724b435e47cb15add0ccfc7a5a7171905f1ff2
Stephane Martin <stephane.martin_github@vesperal.eu>
2018-08-14 00:36:46 +0200

    keep track of outgoing TCP destinations and automatically close them on stopping

--- destination.go
+++ destination.go
@@ -2,50 +2,77 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"net"
-	"os"
+	"sync"
+
+	"github.com/inconshreveable/log15"
 )
 
 type tcpDestination struct {
-	hostport   string
-	maxUploads uint
-	uploads    chan struct{}
-	done       <-chan struct{}
+	hostport    string
+	maxUploads  uint
+	uploads     chan struct{}
+	done        <-chan struct{}
+	connections map[net.Conn]bool
+	logger      log15.Logger
+	sync.Mutex
 }
 
 // TODO: support round-robin list of hostports?
-func newDestination(done <-chan struct{}, hostport string, maxUps uint) *tcpDestination {
-	return &tcpDestination{
-		hostport:   hostport,
-		maxUploads: maxUps,
-		uploads:    make(chan struct{}, maxUps),
-		done:       done,
+func newDestination(done <-chan struct{}, hostport string, maxUps uint, logger log15.Logger) *tcpDestination {
+	d := &tcpDestination{
+		hostport:    hostport,
+		maxUploads:  maxUps,
+		uploads:     make(chan struct{}, maxUps),
+		done:        done,
+		connections: make(map[net.Conn]bool),
+		logger:      logger,
 	}
+	go func() {
+		<-done
+		d.logger.Debug("releaseAllConns")
+		d.Lock()
+		for c := range d.connections {
+			d.logger.Debug("releaseAllConns: release one connection")
+			c.Close()
+			delete(d.connections, c)
+			<-d.uploads
+		}
+		d.Unlock()
+	}()
+	return d
 }
 
 func (d *tcpDestination) getConn() (net.Conn, error) {
+	d.logger.Debug("getConn")
 	select {
 	case <-d.done:
 		return nil, context.Canceled
 	case d.uploads <- struct{}{}:
-		fmt.Fprintln(os.Stderr, "take conn!")
 	}
 	c, err := net.Dial("tcp", d.hostport)
 	if err != nil {
 		<-d.uploads
-		fmt.Fprintln(os.Stderr, "release conn!")
 		return nil, err
 	}
+	d.Lock()
+	d.connections[c] = true
+	d.Unlock()
 	return c, nil
 }
 
-func (d *tcpDestination) releaseConn(conn net.Conn) error {
-	if conn == nil {
+func (d *tcpDestination) releaseConn(c net.Conn) error {
+	d.logger.Debug("releaseConn")
+	if c == nil {
 		return nil
 	}
-	err := conn.Close()
-	<-d.uploads
-	fmt.Fprintln(os.Stderr, "release conn!")
+	var err error
+	d.Lock()
+	if _, ok := d.connections[c]; ok {
+		err = c.Close()
+		delete(d.connections, c)
+		<-d.uploads
+	}
+	d.Unlock()
 	return err
 }
